835741b83d9a48d618f2847584e2bccafd1527db
Harini Kanugula <kanugulaharini2@gmail.com>
2025-07-19 21:20:46 +0530

    Added Addition.java Program

--- Addition.java
+++ Addition.java
@@ -4,6 +4,9 @@ return a+b;
 }
 public static void main(String[] args){
 int sum=add(25,25);
+int diff=Subtraction.subtract(30,20);
 System.out.println("sum of 25 and 25 is :"+sum);
+System.out.println("sub of 30 and 20 is :"+diff);
 }
 }
+//set PATH=C:\Program Files\Java\jdk-21\bin;%PATH%
