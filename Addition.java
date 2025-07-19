500f4871f2654af82470e85f007c671bc52bd407
Harini Kanugula <kanugulaharini2@gmail.com>
2025-07-19 20:25:11 +0530

    Added Addition.java Program

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Addition{
+public static int add(int a,int b){
+return a+b;
+}
+public static void main(String[] args){
+int sum=add(25,25);
+System.out.println("sum of 25 and 25 is :"+sum);
+}
+}
